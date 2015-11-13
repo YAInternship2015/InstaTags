263018ca3ceabb62909565ebaec3f5802a260a9d
Dmitriy Demchenko <organization_98@yahoo.com>
2015-11-13 12:04:15 +0200

    DDInputValidator fixed

--- InstaTags/Classes/OtherClasses/InputValidator/DDInputValidator.m
+++ InstaTags/Classes/OtherClasses/InputValidator/DDInputValidator.m
@@ -13,25 +13,18 @@
 static NSString *const RegularExpression = @"[\\w\\s]{3,100}";
 
 + (BOOL)validateInputString:(NSString *)string error:(NSError **)error {
-    
     NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES[cd] %@", RegularExpression];
     BOOL success = [predicate evaluateWithObject:string];
-    
     NSString *errorMessage;
     NSInteger errorCode = 0;
-    
     if (!success) {
         errorMessage = [@"Tag can't be less than 3 characters." localized];
         errorCode = 9000;
-        
         NSDictionary *userInfo = @{NSLocalizedDescriptionKey : errorMessage};
-        
         if (error != NULL) {
             *error = [NSError errorWithDomain:@"com.dmitriy.demchenko.InstaTags" code:errorCode userInfo:userInfo];
         }
-        
         [[[UIAlertView alloc] initWithTitle:[@"Message" localized] message:userInfo[NSLocalizedDescriptionKey] delegate:nil cancelButtonTitle:[@"Dismiss" localized] otherButtonTitles:nil] show];
-        
         return NO;
     }
     return YES;
